3c8944f8cd5684789fac87bd9864b7df120fb744
Tri Pham <techcaotri@gmail.com>
2024-08-28 16:03:45 +0700

    Enable kitty graphics

--- config/general.lua
+++ config/general.lua
@@ -1,46 +1,53 @@
+local platform = require('utils.platform')()
+
 return {
-   -- behaviours
-   automatically_reload_config = true,
-   exit_behavior = 'CloseOnCleanExit', -- if the shell program exited with a successful status
-   status_update_interval = 1000,
+  -- behaviours
+  automatically_reload_config = true,
+  exit_behavior = 'CloseOnCleanExit', -- if the shell program exited with a successful status
+  status_update_interval = 1000,
 
-   scrollback_lines = 5000,
+  scrollback_lines = 5000,
 
-   hyperlink_rules = {
-      -- Matches: a URL in parens: (URL)
-      {
-         regex = '\\((\\w+://\\S+)\\)',
-         format = '$1',
-         highlight = 1,
-      },
-      -- Matches: a URL in brackets: [URL]
-      {
-         regex = '\\[(\\w+://\\S+)\\]',
-         format = '$1',
-         highlight = 1,
-      },
-      -- Matches: a URL in curly braces: {URL}
-      {
-         regex = '\\{(\\w+://\\S+)\\}',
-         format = '$1',
-         highlight = 1,
-      },
-      -- Matches: a URL in angle brackets: <URL>
-      {
-         regex = '<(\\w+://\\S+)>',
-         format = '$1',
-         highlight = 1,
-      },
-      -- Then handle URLs not wrapped in brackets
-      {
-         regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-         format = '$0',
-      },
-      -- implicit mailto link
-      {
-         regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
-         format = 'mailto:$0',
-      },
-   },
-   ssh_backend = "Ssh2",
+  hyperlink_rules = {
+    -- Matches: a URL in parens: (URL)
+    {
+      regex = '\\((\\w+://\\S+)\\)',
+      format = '$1',
+      highlight = 1,
+    },
+    -- Matches: a URL in brackets: [URL]
+    {
+      regex = '\\[(\\w+://\\S+)\\]',
+      format = '$1',
+      highlight = 1,
+    },
+    -- Matches: a URL in curly braces: {URL}
+    {
+      regex = '\\{(\\w+://\\S+)\\}',
+      format = '$1',
+      highlight = 1,
+    },
+    -- Matches: a URL in angle brackets: <URL>
+    {
+      regex = '<(\\w+://\\S+)>',
+      format = '$1',
+      highlight = 1,
+    },
+    -- Then handle URLs not wrapped in brackets
+    {
+      regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+      format = '$0',
+    },
+    -- implicit mailto link
+    {
+      regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
+      format = 'mailto:$0',
+    },
+  },
+  ssh_backend = "Ssh2",
+  set_environment_variables = platform.is_win and {
+    WEZTERM_WSL="1",
+    WSLENV="WEZTERM_WSL",
+  } or {},
+  enable_kitty_graphics=true,
 }
