37767f9ee6f8d43f9d4cd0b84f39819b8a818c85
Tri Pham <techcaotri@gmail.com>
2024-08-31 10:01:23 +0700

    Add 2 menu items to start WSL:Ubuntu with fish and tmux-fish

--- config/domains.lua
+++ config/domains.lua
@@ -8,13 +8,20 @@ return {
   -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
   wsl_domains = {
     {
-      name = 'WSL:Ubuntu',
+      name = 'WSL:Ubuntu-tmux-fish',
       distribution = 'Ubuntu',
       username = 'tripham',
       default_cwd = '~',
       default_prog = { '/home/tripham/bin/start_tmux_session_fish.sh' },
     },
+    {
+      name = 'WSL:Ubuntu',
+      distribution = 'Ubuntu',
+      username = 'tripham',
+      default_cwd = '~',
+      default_prog = { 'fish' },
+    },
   },
   -- change config now
-  default_domain = 'WSL:Ubuntu'
+  default_domain = 'WSL:Ubuntu-tmux-fish'
 }
